3534ebd610c5d707b52e3d78b882c7bd2657cf4f
Abd Alrahman Ismaik <abdalrahman.ismaik@gmail.com>
2025-06-04 01:26:49 +0400

    Add files via upload

---
+++ EmotionDetection/emotion_detection.py
@@ -0,0 +1,29 @@
+from . import emotion_detectionimport requests
+import json
+
+def emotion_detector(text_to_analyze):
+    URL = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
+    Headers = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
+    Input_json = { "raw_document": { "text": text_to_analyze } }    
+    
+    response = requests.post(URL, json= Input_json, headers=Headers)
+    reresponse_dict = response.json()
+
+    if reresponse_dict['emotionPredictions'] is not None:
+        emotions = reresponse_dict['emotionPredictions'][0]['emotion']
+        anger = emotions['anger']
+        disgust = emotions['disgust']
+        fear = emotions['fear']
+        joy = emotions['joy']
+        sadness = emotions['sadness']
+        dominant_emotion = max(emotions, key=emotions.get)
+        text = {
+            'anger': anger,
+            'disgust': disgust,
+            'fear': fear,
+            'joy': joy,
+            'sadness': sadness,
+            'dominant_emotion': dominant_emotion
+        }
+
+    return text 
