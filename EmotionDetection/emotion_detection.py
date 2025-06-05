c553c56b4fef3bec065b55cce079b0d796e66b69
Abd Alrahman Ismaik <abdalrahman.ismaik@gmail.com>
2025-06-05 15:07:17 +0400

    Update emotion_detection.py

--- EmotionDetection/emotion_detection.py
+++ EmotionDetection/emotion_detection.py
@@ -1,4 +1,4 @@
-from . import emotion_detectionimport requests
+import requests
 import json
 
 def emotion_detector(text_to_analyze):
@@ -6,24 +6,31 @@ def emotion_detector(text_to_analyze):
     Headers = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
     Input_json = { "raw_document": { "text": text_to_analyze } }    
     
-    response = requests.post(URL, json= Input_json, headers=Headers)
+    response = requests.post(URL, json=Input_json, headers=Headers)
+    text = {
+        'anger': None,
+        'disgust': None,
+        'fear': None,
+        'joy': None,
+        'sadness': None,
+        'dominant_emotion': None
+    }
+
+    # Handle blank input or server error
+    if response.status_code == 400 or not text_to_analyze.strip():
+        return text
+    
     reresponse_dict = response.json()
 
     if reresponse_dict['emotionPredictions'] is not None:
         emotions = reresponse_dict['emotionPredictions'][0]['emotion']
-        anger = emotions['anger']
-        disgust = emotions['disgust']
-        fear = emotions['fear']
-        joy = emotions['joy']
-        sadness = emotions['sadness']
-        dominant_emotion = max(emotions, key=emotions.get)
         text = {
-            'anger': anger,
-            'disgust': disgust,
-            'fear': fear,
-            'joy': joy,
-            'sadness': sadness,
-            'dominant_emotion': dominant_emotion
+            'anger': emotions['anger'],
+            'disgust': emotions['disgust'],
+            'fear': emotions['fear'],
+            'joy': emotions['joy'],
+            'sadness': emotions['sadness'],
+            'dominant_emotion': max(emotions, key=emotions.get)
         }
 
     return text 
